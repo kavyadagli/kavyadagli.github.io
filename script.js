556cafb6bb2cacdaf1f17f17572df48c035ea243
kavyadagli <kdagli@terpmail.umd.edu>
2018-06-18 12:15:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//activate slider
+
+$('.nav-side .nav-toggle').on('click', function(e) {
+  e.preventDefault();
+  $(this).parent().toggleClass('nav-open');
+});
+
+
+
+//change theme on click
+
+var count = 0
+
+document.getElementById('trigger').onclick = function () {
+    count = count + 1;
+    if (count%2!=0){
+      document.getElementById('default').href = 'changeTheme.css';
+
+    } else {
+       document.getElementById('default').href = 'main.css';
+    }
+};
