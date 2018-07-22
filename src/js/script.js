991ad3b3769fc293e2b1dddae70bdf0a034b3d8b
kdagli <kavyadagli@yahoo.com>
2018-07-22 10:39:38 -0400

    organized folders

--- src/js/script.js
+++ src/js/script.js
@@ -14,9 +14,9 @@ var count = 0
 document.getElementById('trigger').onclick = function () {
     count = count + 1;
     if (count%2!=0){
-      document.getElementById('default').href = 'changeTheme.css';
+      document.getElementById('default').href = '../src/css/changeTheme.css';
 
     } else {
-       document.getElementById('default').href = 'main.css';
+       document.getElementById('default').href = '../src/css/main.css';
     }
 };
